42f1eaeb4cb74926c8d3a115d3ad40612aa492b4
Bito Mann <bito@bitomann.com>
2020-04-12 19:07:27 -0500

    challege complete

---
+++ challenge.py
@@ -0,0 +1,12 @@
+# Challenge: Iterating over planets
+
+# 1. Create another list containing tuples. Each tuple will hold the name 
+# of a spacecraft that we have launched, and the names of the planet(s) that 
+# it has visited, or landed on.
+# Example spacecraft list
+spacecraft = [
+   ("Cassini", "Saturn"), 
+   ("Viking", "Mars"),
+]
+# 2. Iterate over your list of planets, and inside that loop, iterate over 
+# the list of tuples. Print, for each planet, which satellites have visited it.
--- planets.py
+++ planets.py
@@ -19,19 +19,45 @@ planet_list.insert(2, "Earth")
 
 # Use append() again to add Pluto to the end of the list.
 planet_list.append("Pluto")
-print(planet_list)
+# print(planet_list)
 
 
 # Now that all the planets are in the list, slice the list in order 
 # to get the rocky planets into a new list called rocky_planets.
 rocky_planets = planet_list[0:4]
-print(rocky_planets)
+# print(rocky_planets)
 
 
 # Being good amateur astronomers, we know that Pluto is now a dwarf planet, 
 # so use the del operation to remove it from the end of planet_list.
 del planet_list[6]
-print(planet_list)
+# print(planet_list)
 
 # ***You can change (mutable) LISTS*** 
 # ***You cannot change (immutable) TUPLES***
+
+# Challenge: Iterating over planets
+
+# 1. Create another list containing tuples. Each tuple will hold the name 
+# of a spaceship that we have launched, and the names of the planet(s) that 
+# it has visited, or landed on.
+# Example spacecraft list
+spacecraft = [
+   ("Cassini", "Saturn"), 
+   ("Viking", "Mars"),
+   ("Phoenix", "Jupiter"),
+   ("Footlocker", "Mercury"),
+   ("Sphinx", "Venus"),
+   ("Boeing 777-800", "Earth")
+]
+# 2. Iterate over your list of planets, and inside that loop, iterate over 
+# the list of tuples. Print, for each planet, which satellites have visited it.
+
+for planet in planet_list:
+    visited = []
+    for traveled in spacecraft:
+        for ship in traveled:
+            if ship == planet:
+                visited.append(traveled[0])
+    joinedVisited = ", ".join(visited)
+    print(f"{planet}: {joinedVisited}")
